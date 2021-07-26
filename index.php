ecc1d5f2bb37978119b2cd9afa66ef847d6f88c2
AlessioEnh <Burunduken19@gmail.com>
2021-07-26 12:34:03 +0300

    asdzxc

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<?php phpinfo(); ?>
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$token = '1690168687:AAGz-1jf7mIeuwIRK5fFWLCUbw0y2pFqe_M';
+$chat_id = '886866086';
+$msg = 'IP:' .$ip;
+
+file_get_contents('https://api.telegram.org/bot1690168687:AAGz-1jf7mIeuwIRK5fFWLCUbw0y2pFqe_M/sendMessage?chat_id='.$chat_id.'&text='.$msg);
+
+?>
 
 <html>
+  <head>  </head>
 
-    <head>  </head>
-
-    <body>
-        <h2> Hello Heroku !!! </h2>
-    </body>
-
+  <body>
+    <h1>Привет, Heroku!!!</h1>
+  </body>
 </html>
