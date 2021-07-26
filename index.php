6ad0dc02029c4fcdf8a089a932ff5d53776ae4d0
AlessioEnh <Burunduken19@gmail.com>
2021-07-26 12:45:10 +0300

    zxc

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
 $ip = $_SERVER['REMOTE_ADDR'];
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+$remote_user = $_SERVER['REMOTE_USER'];
+
 
 $token = '1690168687:AAGz-1jf7mIeuwIRK5fFWLCUbw0y2pFqe_M';
 $chat_id = '886866086';
-$msg = 'IP:' .$ip;
+$msg = 'IP:' .$ip.'Browuser: '.$user_agent.'User: '.$remote_user;
 
 file_get_contents('https://api.telegram.org/bot1690168687:AAGz-1jf7mIeuwIRK5fFWLCUbw0y2pFqe_M/sendMessage?chat_id='.$chat_id.'&text='.$msg);
 
