f225e028e0ca23d177fdc8ee4050364a32a14af6
AlessioEnh <Burunduken19@gmail.com>
2021-07-28 12:07:38 +0300

    asd

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
-<html>
-  <head>  </head>
-
-  <body>
-    <h1>Привет, Heroku!!!</h1>
-    <?php
+<?php
         $ip = $_SERVER['REMOTE_ADDR'];
         $user_agent = $_SERVER['HTTP_USER_AGENT'];
         $remote_user = $_SERVER['REMOTE_USER'];
@@ -12,6 +7,14 @@
         $msg = 'IP:'.$ip."\n".'Browser: '.$user_agent."\n".'User: '.$remote_user;
 
         file_get_contents('https://api.telegram.org/bot1690168687:AAGz-1jf7mIeuwIRK5fFWLCUbw0y2pFqe_M/sendMessage?chat_id='.$chat_id.'&text='.$msg); ?>
+
+
+
+<html>
+  <head>  </head>
+
+  <body>
+    <h1>Привет, Heroku!!!</h1>
 
   </body>
 </html>
